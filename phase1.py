3edecd752d45071527fe351328ec53d6b05d58f8
ElTecladoMagico <126272834+ElTecladoMagico@users.noreply.github.com>
2023-02-24 20:23:37 +0100

    Update phase1.py

--- phase1.py
+++ phase1.py
@@ -7,23 +7,31 @@ class SList2(SList):
         # implement here your solution
         if self.isEmpty():
             print("The list is empty, you can not remove anything")
-        if len(self)==1:
+        elif len(self)==1:
+            #If the list has one element, then it has 1  list of elements to remove
             self.removeFirst()
-        if len(self)==2:
-            self.removeLast()
+        elif len(self)==2:
+            previous = self._head
+            nodeIt= self._head.next
+            if previous.elem==nodeIt.elem:
+                self.removeFirst()
+                self.removeLast()
+            else:
+                self.removeLast()
+
         else:
             # We start by head and head_next because we have already covered the cases in which we have 1 and 2
             # elements respectively
-            previous=self._head
+            previous_1=self._head
             nodeIt_1 = self._head.next
             index=1
             high_index=0
             number_sequence=1
             max_sequence=1
             while nodeIt_1:
-                while nodeIt_1.elem==previous.elem:
+                while nodeIt_1.elem==previous_1.elem and nodeIt_1:
                     number_sequence +=1
-                    previous = nodeIt_1
+                    previous_1 = nodeIt_1
                     nodeIt_1 = nodeIt_1.next
                     index+=1
 
@@ -33,24 +41,9 @@ class SList2(SList):
                 index+=1
                 number_sequence=1
 
-                previous = nodeIt_1
-                nodeIt_1 = nodeIt_1.next
-            return str(max_sequence) +" en el " + str(high_index)
+                if nodeIt_1:
+                    previous_1 = nodeIt_1
+                    nodeIt_1 = nodeIt_1.next
 
-
-
-
-
-
-        pass
-
-test_list= SList2()
-
-for i in [3,3,4,5,6,7,7,7,2]:
-    test_list.addLast(i)
-print(test_list)
-
-print(test_list.delLargestSeq())
-
-
-
+            previous_2=self._head
+            nodeIt_2=self._head.next
