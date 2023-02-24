bf054f42bdb21774c31fc2b28d63d4304f5f4038
ElTecladoMagico <126272834+ElTecladoMagico@users.noreply.github.com>
2023-02-24 20:23:11 +0100

    Update phase1.py

--- phase1.py
+++ phase1.py
@@ -25,16 +25,18 @@ class SList2(SList):
                     number_sequence +=1
                     previous = nodeIt_1
                     nodeIt_1 = nodeIt_1.next
+                    index+=1
 
                 if max_sequence <= number_sequence:
                     max_sequence = number_sequence
-                    high_index=index - max_sequence
-
+                    high_index = index - max_sequence
+                index+=1
                 number_sequence=1
 
                 previous = nodeIt_1
                 nodeIt_1 = nodeIt_1.next
-            return str(max_sequence)
+            return str(max_sequence) +" en el " + str(high_index)
+
 
 
 
@@ -44,41 +46,11 @@ class SList2(SList):
 
 test_list= SList2()
 
-for i in [3,3,4,5,7,7,7,2]:
+for i in [3,3,4,5,6,7,7,7,2]:
     test_list.addLast(i)
 print(test_list)
 
 print(test_list.delLargestSeq())
 
 
-# def delLargestSeq(self):
-#     # implement here your solution
-#     if self.isEmpty():
-#         print("The list is empty, you can not remove anything")
-#     if len(self) == 1:
-#         self.removeFirst()
-#     if len(self) == 2:
-#         self.removeLast()
-#     else:
-#         # We start by head and head_next because we have already covered the cases in which we have 1 and 2
-#         # elements respectively
-#         previous = self._head
-#         nodeIt_1 = self._head.next
-#         index = 1
-#         number_sequence = 1
-#         max_sequence = 1
-#         while nodeIt_1:
-#             while nodeIt_1.elem == previous.elem:
-#                 number_sequence += 1
-#                 previous = nodeIt_1
-#                 nodeIt_1 = nodeIt_1.next
-#
-#             if max_sequence <= number_sequence:
-#                 max_sequence = number_sequence
-#
-#             number_sequence = 1
-#
-#             previous = nodeIt_1
-#             nodeIt_1 = nodeIt_1.next
-#         return str(max_sequence)
 
