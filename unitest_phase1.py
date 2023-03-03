a9fa8f326bfb6159f040af1d725ff2586703cc19
Est3b4nG <104792566+Est3b4nG@users.noreply.github.com>
2023-03-03 09:53:49 +0100

    Update unitest_phase1.py

--- unitest_phase1.py
+++ unitest_phase1.py
@@ -3,7 +3,6 @@ import unittest
 from phase1 import SList2
 
 
-
 class Test_for_delLargestSeq(unittest.TestCase):
         #This Class will check the possible cases where we could have errors for the function delLargestSequence
 
@@ -16,8 +15,8 @@ class Test_for_delLargestSeq(unittest.TestCase):
         """This function checks if the method works for an empty list"""
         self.input.delLargestSeq()
         #The assertEqual method help us to see if our expected value is the same that we got as result
-        self.assertEqual(len(self.input),0)
-        self.assertEqual(str(self.input), "")
+        self.assertEqual(0,len(self.input))
+        self.assertEqual("",str(self.input))
 
     def test_for_len_1(self):
         """This method help us check if the function works for list of 1 element"""
@@ -26,7 +25,7 @@ class Test_for_delLargestSeq(unittest.TestCase):
         self.input.addFirst(1)
         self.input.delLargestSeq()
         self.expected= ""
-        self.assertEqual(str(self.input),str(self.expected))
+        self.assertEqual(str(self.expected),str(self.input))
 
     def test_for_len_2_equal(self):
         """This method help us check if the function works for list of 2 elements that are equal"""
@@ -37,7 +36,7 @@ class Test_for_delLargestSeq(unittest.TestCase):
         self.input.delLargestSeq()
         #We expect the method eliminates all the list
         self.expected= ""
-        self.assertEqual(str(self.input),str(self.expected))
+        self.assertEqual(str(self.expected),str(self.input))
 
     def test_for_len_2_distinct(self):
         """Checks if the method works for lists of two nodes of different values"""
@@ -48,7 +47,7 @@ class Test_for_delLargestSeq(unittest.TestCase):
         self.input.delLargestSeq()
         #We expect that the method eliminates the last element of the list
         self.expected= 1
-        self.assertEqual(str(self.input),str(self.expected))
+        self.assertEqual(str(self.expected),str(self.input),)
 
 
 
@@ -61,7 +60,7 @@ class Test_for_delLargestSeq(unittest.TestCase):
         self.input.delLargestSeq()
         #We expect that it eliminates the sequence
         self.expected= "1,2,2,3"
-        self.assertEqual(str(self.input),str(self.expected))
+        self.assertEqual(str(self.expected),str(self.input),)
 
     def test_for_big_sequence(self):
         """Checks if the method works for lists with only one sequence"""
@@ -72,7 +71,7 @@ class Test_for_delLargestSeq(unittest.TestCase):
         self.input.delLargestSeq()
         #We expect that it eliminates the big sequence
         self.expected= "1,2,2,3"
-        self.assertEqual(str(self.input),str(self.expected))
+        self.assertEqual(str(self.expected),str(self.input),)
 
 
     def test_for_2_size_sequences(self):
@@ -84,7 +83,7 @@ class Test_for_delLargestSeq(unittest.TestCase):
         self.input.delLargestSeq()
         #We expect that it eliminates the biggest sequence
         self.expected= "1,3,3,3"
-        self.assertEqual(str(self.input),str(self.expected))
+        self.assertEqual(str(self.expected),str(self.input),)
 
 
     def test_for_3_size_sequences(self):
@@ -96,7 +95,7 @@ class Test_for_delLargestSeq(unittest.TestCase):
         self.input.delLargestSeq()
         #We expect that it eliminates the biggest sequence
         self.expected= "1,3,3,3,6,5,6,2,3,4,4,4,4,2"
-        self.assertEqual(str(self.input),str(self.expected))
+        self.assertEqual(str(self.expected),str(self.input))
 
     def test_for_same_size_sequences(self):
         """Checks if the method works for list with two sequences of same size"""
@@ -107,7 +106,7 @@ class Test_for_delLargestSeq(unittest.TestCase):
         self.input.delLargestSeq()
         #We expect that it eliminates from both sequences the one that is more to the right
         self.expected= "1,3,3,3"
-        self.assertEqual(str(self.input),str(self.expected))
+        self.assertEqual(str(self.expected),str(self.input))
 
     def test_for_3_same_sequences(self):
         """Checks if the method works for lists with only one sequence"""
@@ -118,29 +117,29 @@ class Test_for_delLargestSeq(unittest.TestCase):
         self.input.delLargestSeq()
         #We expect that it eliminates the biggest sequence
         self.expected= "1,3,3,3,4,4,4,1"
-        self.assertEqual(str(self.input),str(self.expected))
+        self.assertEqual(str(self.expected),str(self.input))
 
     def test_for_begginning(self):
         """Checks if the method works for lists that start with the biggest sequence"""
         self.expected = SList2()
-        for i in [3,3,3,4,4,3]:
+        for i in [3,3,3,3,4,4,3]:
             self.input.addLast(i)
 
         self.input.delLargestSeq()
         #We expect that it eliminates the biggest sequence, that is the first one
         self.expected= "4,4,3"
-        self.assertEqual(str(self.input),str(self.expected))
+        self.assertEqual(str(self.expected),str(self.input))
 
     def test_for_ending(self):
         """Checks if the method works for lists that end in the biggest sequence"""
         self.expected = SList2()
-        for i in [1,3,3,4,6,6,6,6]:
+        for i in [1,3,3,4,6,6,6,6,6,6]:
             self.input.addLast(i)
 
         self.input.delLargestSeq()
         #We expect that it eliminates the biggest sequence, that is the last one
         self.expected= "1,3,3,4"
-        self.assertEqual(str(self.input),str(self.expected))
+        self.assertEqual(str(self.expected),str(self.input))
 
     def test_for_all_same(self):
         """Checks if the method works for lists with only the same element"""
@@ -151,4 +150,4 @@ class Test_for_delLargestSeq(unittest.TestCase):
         self.input.delLargestSeq()
         # We expect that it eliminates the biggest sequence
         self.expected = ""
-        self.assertEqual(str(self.input), str(self.expected))
+        self.assertEqual(str(self.expected),str(self.input))
