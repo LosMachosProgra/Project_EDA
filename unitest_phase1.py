052e91d1d0bbe2ac2c26b544cd8974a68d74f7c6
Est3b4nG <104792566+Est3b4nG@users.noreply.github.com>
2023-03-07 19:44:36 +0100

    Update unitest_phase1.py

--- unitest_phase1.py
+++ unitest_phase1.py
@@ -1,14 +1,14 @@
-
 import unittest
 from phase1 import SList2
 
 
-class Test_for_delLargestSeq(unittest.TestCase):
+class Tests(unittest.TestCase):
         #This Class will check the possible cases where we could have errors for the function delLargestSequence
 
     def setUp(self):
         #This is the list that we're going to use as base in most of the tests
         self.input=SList2()
+        self.expected = SList2()
 
 
     def test_for_empty(self):
@@ -20,7 +20,6 @@ class Test_for_delLargestSeq(unittest.TestCase):
 
     def test_for_len_1(self):
         """This method help us check if the function works for list of 1 element"""
-        self.expected = SList2()
         #We add 1 element as we want to prove it works for 1 node lists.
         self.input.addFirst(1)
         self.input.delLargestSeq()
@@ -29,7 +28,6 @@ class Test_for_delLargestSeq(unittest.TestCase):
 
     def test_for_len_2_equal(self):
         """This method help us check if the function works for list of 2 elements that are equal"""
-        self.expected = SList2()
         #The loop was made with the objective of adding two nodes to the list
         for i in range(2):
             self.input.addFirst(1)
@@ -40,7 +38,6 @@ class Test_for_delLargestSeq(unittest.TestCase):
 
     def test_for_len_2_distinct(self):
         """Checks if the method works for lists of two nodes of different values"""
-        self.expected = SList2()
         #The loop was made with the objective of adding two nodes to the list
         self.input.addFirst(1)
         self.input.addLast(2)
@@ -53,7 +50,6 @@ class Test_for_delLargestSeq(unittest.TestCase):
 
     def test_for_1_sequence(self):
         """Checks if the method works for lists with only one sequence"""
-        self.expected = SList2()
         for i in [1,2,4,4,4,2,3]:
             self.input.addLast(i)
 
@@ -64,7 +60,6 @@ class Test_for_delLargestSeq(unittest.TestCase):
 
     def test_for_big_sequence(self):
         """Checks if the method works for lists with only one sequence"""
-        self.expected = SList2()
         for i in [1,2,4,4,4,4,4,4,4,4,2,3]:
             self.input.addLast(i)
 
@@ -76,7 +71,6 @@ class Test_for_delLargestSeq(unittest.TestCase):
 
     def test_for_2_size_sequences(self):
         """Checks if the method works for two sequences of different sizes"""
-        self.expected = SList2()
         for i in [1,3,3,4,4,4,3]:
             self.input.addLast(i)
 
@@ -88,7 +82,6 @@ class Test_for_delLargestSeq(unittest.TestCase):
 
     def test_for_3_size_sequences(self):
         """Checks if the method works for 3 lists of different sizes"""
-        self.expected = SList2()
         for i in [1,3,3,3,6,5,6,4,4,4,4,4,4,4,4,2,3,4,4,4,4,2]:
             self.input.addLast(i)
 
@@ -99,7 +92,6 @@ class Test_for_delLargestSeq(unittest.TestCase):
 
     def test_for_same_size_sequences(self):
         """Checks if the method works for list with two sequences of same size"""
-        self.expected = SList2()
         for i in [1,3,3,4,4,3]:
             self.input.addLast(i)
 
@@ -110,7 +102,6 @@ class Test_for_delLargestSeq(unittest.TestCase):
 
     def test_for_3_same_sequences(self):
         """Checks if the method works for lists with only one sequence"""
-        self.expected = SList2()
         for i in [1,3,3,3,4,4,4,2,2,2,1]:
             self.input.addLast(i)
 
@@ -121,7 +112,6 @@ class Test_for_delLargestSeq(unittest.TestCase):
 
     def test_for_begginning(self):
         """Checks if the method works for lists that start with the biggest sequence"""
-        self.expected = SList2()
         for i in [3,3,3,3,4,4,3]:
             self.input.addLast(i)
 
@@ -132,7 +122,6 @@ class Test_for_delLargestSeq(unittest.TestCase):
 
     def test_for_ending(self):
         """Checks if the method works for lists that end in the biggest sequence"""
-        self.expected = SList2()
         for i in [1,3,3,4,6,6,6,6,6,6]:
             self.input.addLast(i)
 
@@ -143,11 +132,27 @@ class Test_for_delLargestSeq(unittest.TestCase):
 
     def test_for_all_same(self):
         """Checks if the method works for lists with only the same element"""
-        self.expected = SList2()
         for i in [6, 6, 6, 6]:
             self.input.addLast(i)
-
         self.input.delLargestSeq()
         # We expect that it eliminates the biggest sequence
         self.expected = ""
         self.assertEqual(str(self.expected),str(self.input))
+
+    def test_for_complete_loop(self):
+        """Checks if the method of fixing loop works for a loop that end in the last node and starts at self_head"""
+        for i in [1, 2, 3, 4, 5, 6]:
+            self.input.addLast(i)
+        self.input.create_loop(0)
+        self.input.fix_loop()
+        self.expected="1,2,3,4,5,6"
+        self.assertEqual(str(self.expected),str(self.input))
+
+    # def test_for_loop_at_beggining(self):
+    #     """Checks if the method of fixing loop works for a loop that end in the last node and starts at self_head"""
+    #     for i in [1, 2, 3, 4, 5, 6, 7, 8]:
+    #         self.input.addLast(i)
+    #     self.input.create_loop(2)
+    #     self.input.fix_loop()
+    #     self.expected="1,2,3,4,5,6,7,8"
+    #     self.assertEqual(str(self.expected),str(self.input))
