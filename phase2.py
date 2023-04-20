c57f5755f3396d22af5a4a1ad0f4db3d0eab28b1
Est3b4nG <104792566+Est3b4nG@users.noreply.github.com>
2023-04-20 18:28:23 +0200

    Update phase2.py

--- phase2.py
+++ phase2.py
@@ -6,17 +6,13 @@ Estéban Gómez Buitrago
 Pedro Gabriel Mantese Masegosa
 """
 
-# Classes provided by EDA Team
 from bintree import BinaryNode
 from bst import BinarySearchTree
 
-
-
-# Exercise #1
 class BST2(BinarySearchTree):
 
     def find_dist_k(self, n: int, k: int) -> list:
-        output=[]
+        output = []
 
         if k < 1:
             raise TypeError("k must be a positive integer.")
@@ -30,31 +26,37 @@ class BST2(BinarySearchTree):
 
         # depth_to_node, node_n = self.depth_to_node()
 
-        self.move_down_k_positions(node,k, output)
+        self.move_down_k_positions(node, k, output)
         # To obtain more outputs, we have to climb up the tree k positions, for that we simply go down less positions
         # from the root using the searchit method.
+        self.upwards_output(self.root, node, output)
+
+
         return output
+
 
     # Since we can only use .right and .left, we will create a parent of the node by linking it to the previous
     # element. This will allow us to track our steps easier.
 
     # With this search method we will move k positions down in the tree and store the last values in a DList.
-    def move_down_k_positions(self, node:BinaryNode, k: int, output:list) :
+    def move_down_k_positions(self, node: BinaryNode, k: int, output: list):
         """Returns the elements k steps further down the tree."""
         if not node:
             return
 
         if k > 0:
-            self.move_down_k_positions(node.left,k-1,output)
-            self.move_down_k_positions(node.right, k-1,output)
+            self.move_down_k_positions(node.left, k - 1, output)
+            self.move_down_k_positions(node.right, k - 1, output)
 
         if k == 0:
             output.append(node.elem)
-        
-    def search_from_current_node(self, elem):
-        return self._search_from_current_node(self.node, elem)
 
-    def _search_from_current_node(self, node, elem):
+
+    #def search_from_current_node(self, elem):
+     #   return self._search_from_current_node(self.node_it, elem)
+
+
+    def _search_from_current_node(self, node:BinaryNode, elem:object):
         if node is None or node.elem == elem:
             return node
         elif elem < node.elem:
@@ -62,75 +64,36 @@ class BST2(BinarySearchTree):
         elif elem > node.elem:
             return self._search_from_current_node(node.right, elem)
 
-    def upwards_output(self) -> str:
-        """This method adds to the output the value of the elements that are directly "k" positions above it."""
-        node_n = self.search(self.n)
-        n_depth = self.depth(node_n)
-        desired_depth = n_depth - self.k
-        if self.depth(self.node) != desired_depth:
-            self.upwards_output(self.node.left)
-            self.upwards_output(self.node.right)
 
-        if self.depth(self.node) == desired_depth:
-            if self.search_from_current_node(self.n) != None:
-                self.output += str(self.node)
-                return self.output
-            if self.search_from_current_node(self.n) == None:
-                return
-    #
-    # def _search(self, node: BinaryNode, elem: object) -> BinaryNode and str:
-    #     """Recursive function"""
-    #     # We will add a counter to know how many steps took us to find the number 'n'.
-    #     downwards_counter = 0
-    #     # We create the DList we will use to store the elements as we go down in the branches.
-    #     # downwards_list = self.DList()
-    #     # We store the values as part of the output.
-    #     output = '['
-    #
-    #     for index in range(0, self.k):
-    #         # We write self.k - 1 because the range goes from 0 to self.k, last not included, so self.k - 1 is the
-    #         # penultimate iteration.
-    #         if node.right is None and node.left is None and index != self.k - 1:
-    #             return node
-    #         elif elem < node.elem and index != self.k - 1:
-    #             return self._search(node.left, elem)
-    #         elif elem > node.elem and index != self.k - 1:
-    #             return self._search(node.right, elem)
-    #         # For this last iteration, we store the elements as part of the output
-    #         elif index == self.k - 1:
-    #             output += str(node) + ', '
-    #         downwards_counter += 1
-    #     return output and downwards_counter
-    # #
-    # def depth_to_node(self, node: BinaryNode, elem: object):
-    #     #This function returns the depth at which the element "n" is.
-    #     node = self._root
-    #     depth_to_node = 0
-    #     if node == None:
-    #         # In this case the tree does not exist or does not have a root.
-    #         return 0
-    #     elif node.elem == elem:
-    #         return depth_to_node, node
-    #     elif elem < node.elem:
-    #         node = node.left
-    #         depth_to_node += 1
-    #     elif elem > node.elem:
-    #         node = node.right
-    #         depth_to_node += 1
-    #
-    # def k_movements(self):
-    #     starting_node = self.node_n
-    #     #As we move in the tree, k_counter will decrease until it reaches 0.
-    #     k_counter = self.k
-    #     #The answers will be writen in the output.
-    #     output = '['
-    #     if k_counter == 0:
-    #         output += self.node.elem
-    #
-    #     # In here we include the possibilities in which it goes down and up.
-    #     if 0 < k < self.node_n.depth_to_node():
-    #         # We go down in the tree k movements
-    #         while k_counter > 0:
+    def upwards_output(self,node_It:BinaryNode, node:BinaryNode, output:list) -> list:
+        """This method adds to the output the value of the elements that are directly "k" positions above it."""
+        if node_It:
+            n_depth = self.depth(node)
+            desired_depth = n_depth - self.k
+            if self.depth(node_It) != desired_depth:
+                self.upwards_output(node_It.left, node, output)
+                self.upwards_output(node_It.right, node,output)
+
+            if self.depth(node_It) == desired_depth:
+                if self._search_from_current_node(node_It, node.elem) != None:
+                    output.append(node_It.elem)
+                    return output
+            
+
+
+#Some usage examples
+if __name__ == '__main__':
+    input_list_01 = [5, 2,3, 1, 7, 9, 6, 23]
+
+    # Build and draw first tree
+    tree1 = BST2()
+    for x in input_list_01:
+        tree1.insert(x)
+    tree1.draw()
+    print(tree1.find_dist_k(7,2))
+
+
+
 # Exercise #2
 
 @property
